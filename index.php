4901c3d8ccbfa31ca9ef68a1479f3e23f593957d
FlyBirdHeight <531020509@qq.com>
2018-01-10 10:14:06 +0800

    ljq

--- index.php
+++ index.php
@@ -3,10 +3,6 @@
 use Workerman\Worker;
 require_once __DIR__.'/Autoloader.php';
 
-$ws_worker = new Worker("tcp://0.0.0.0:2347");
-
-$ws_worker->count = 4;
-
 $global_uid = 0;
 
 // 当客户端连上来时分配uid，并保存连接，并通知所有客户端
@@ -17,11 +13,15 @@ function handle_connection($connection)
     $connection->uid = ++$global_uid;
 }
 
-$ws_worker->onMessage = function($connection, $data)
+// 当客户端发送消息过来时，转发给所有人
+function handle_message($connection, $data)
 {
-    // 向客户端发送hello $data
-    $connection->send("user[{$connection->uid}] said: $data");
-};
+    global $ws_worker;
+    foreach($ws_worker->connections as $conn)
+    {
+        $conn->send("user[{$connection->uid}] said: $data");
+    }
+}
 
 // 当客户端断开时，广播给所有客户端
 function handle_close($connection)
@@ -33,6 +33,9 @@ function handle_close($connection)
     }
 }
 
+$ws_worker = new Worker("tcp://0.0.0.0:2347");
+
+$ws_worker->count = 4;
 $ws_worker->onConnect = 'handle_connection';
 $ws_worker->onMessage = 'handle_message';
 $ws_worker->onClose = 'handle_close';
