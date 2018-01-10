4b20d9ff4caff0ea9a8113b46e1634fb890c3f87
FlyBirdHeight <531020509@qq.com>
2018-01-10 09:03:53 +0800

    ljq

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+use Workerman\Worker;
+require_once __DIR__.'./Autoloader.php';
+
+$ws_worker = new Worker("tcp://0.0.0.0:2347");
+
+$ws_worker->count = 4;
+
+$ws_worker->onMessage = function($connection, $data)
+{
+    // 向客户端发送hello $data
+    $connection->send('hello ' . $data);
+};
+
+
+Worker::runAll();
