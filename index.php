1c987b3666304e76e2cfae710beabdcbc3234098
FlyBirdHeight <531020509@qq.com>
2018-04-08 20:50:11 +0800

    ljq

--- index.php
+++ index.php
@@ -41,8 +41,7 @@ function handle_message($connection, $data)
 {
     global $ws_worker,$clients;
     $data = json_decode($data,true);
-    $data = json_encode($data);
-    print_r($data->type);
+    print_r($data['type']);
 //    $dataInfo = $data.type;
 //    echo $dataInfo;
 //    foreach($ws_worker->connections as $conn)
